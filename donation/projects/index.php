225496bdaa9c5357f9ad8b2e9c6fdf68085b1687
Vega Setiawan <vegasetiawan16@gmail.com>
2024-12-04 13:52:06 +0700

    tolong benerin

--- donation/projects/index.php
+++ donation/projects/index.php
@@ -23,10 +23,10 @@
 
     <header class="flex justify-between items-center p-6 z-50">
         <div class="text-lg font-bold -ml-4 lg:ml-16">
-            <img src="/logo.svg" />
+            <img src="../../logo.svg" />
         </div>
         <nav class="space-x-2 lg:space-x-6 ml-4 flex flex-row ml-auto mr-auto">
-            <a class="text-white hover:text-red-500 text-md lg:text-lg duration-300" href="/">
+            <a class="text-white hover:text-red-500 text-md lg:text-lg duration-300" href="../../">
                 Home
             </a>
             <a class="text-white hover:text-red-500 text-md lg:text-lg duration-300" href="/contact/">
@@ -56,24 +56,64 @@
             </script>
         </nav>
     </header>
-    <div class="bg-[url('/donation.webp')] bg-cover h-screen w-screen max-w-screen brightness-[70%] fixed top-0 -z-10">
+    <div class="bg-[url('../../donation.webp')] bg-cover h-screen w-screen max-w-screen brightness-[70%] fixed top-0 -z-10">
     </div>
     <div>
         <h1 class="text-2xl lg:text-4xl text-center mt-36 lg:mt-72 mb-16">
             Bosan scrolling timeline? Yuk, ganti jadi scrolling kebaikan<br />
             Donasi sekarang dan rasakan sensasi jadi orang baik!
         </h1>
-        <div class="w-[90vw] lg:w-[80vw] bg-[#F8F4E8] mx-auto rounded-t-[35px] px-4 lg:px-16 py-10 text-black">
+        <div class="w-[90vw] lg:w-[90vw] bg-[#F8F4E8] mx-auto rounded-t-[35px] px-4 lg:px-16 py-10 text-black">
             <h1 class="font-bold text-xl lg:text-2xl">Carilah donasi sebanyak mungkin</h1>
             <div class="overflow-x-auto mt-8">
-                    
+                <div id="project-container" class="flex gap-8 cursor-grab active:cursor-grabbing transition-transform duration-300 ease-linear"></div>
             </div>
-
         </div>
     </div>
-    <script>
-
-    </script>
 </body>
-
+<script>
+    fetch(window.location.href.replace("index.php", "") + '/api/projects/')
+        .then(response => response.json())
+        .then(res => {
+            const container = document.getElementById('project-container');
+            container.innerHTML = '';
+            const data = res.data
+            data.forEach(project => {
+                const progress = (project.donation / project.donation_target) * 100;
+                const projectHTML = `
+                    <div class="bg-teal-900 text-white rounded-lg h-[540px]">
+                        <img alt="${project.title}" class="mb-4 rounded-lg w-full max-h-64 object-cover"
+                            src="project${project.id}.webp" />
+                        <div class="py-6 px-9 flex flex-col h-[320px]">
+                            <span class="bg-[#EC5A49] px-4 rounded-full mr-auto">${project.category}</span>
+                            <h3 class="text-xl font-bold my-2 text-left">
+                                ${project.title}
+                            </h3>
+                            <p class="text-lg mb-4 text-left font-light text-ellipsis line-clamp-3">
+                                ${project.description}
+                            </p>
+                            <div class="flex flex-row mt-auto">
+                                <div class="flex flex-col">
+                                    <div class="bg-[#F8F4E8] h-3 w-full rounded-full mb-4">
+                                        <div class="bg-[#EC5A49] h-3 rounded-full" style="width: ${progress}%;"></div>
+                                    </div>
+                                    <p class="text-sm mr-auto">
+                                        Terkumpul Rp. ${parseInt(project.donation).toLocaleString('id-ID')} / Rp. ${parseInt(project.donation_target).toLocaleString('id-ID')}
+                                    </p>
+                                </div>
+                                <div class="flex justify-center items-center ml-auto">
+                                    <a class="bg-[#EC5A49] text-white px-5 py-3 rounded-lg mb-4 inline-flex items-center font-bold hover:opacity-50 active:scale-97 duration-300 lg:mr-auto mx-auto lg:ml-0" 
+                                    href="donation"> Donasi</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`
+                ;
+                container.insertAdjacentHTML('beforeend', projectHTML);
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching project data:', error);
+        });
+</script>
 </html>
